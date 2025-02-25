c455b890d6d12fc41df6e61caccd702354b0e965
Denys Fominykh <fominykhdenis@gmail.com>
2025-02-25 15:56:47 +0200

    change type annotation

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -2,9 +2,9 @@ import type { ReactNode } from 'react';
 import { HeroUIProvider } from '@heroui/system';
 import { SessionProvider } from 'next-auth/react';
 
-type TProvidersProps = Readonly<{
-  children: ReactNode;
-}>;
+type TProvidersProps = {
+  readonly children: ReactNode;
+};
 
 export function Providers({ children }: TProvidersProps) {
   return (
