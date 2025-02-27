8b3b27227ea9a9d30b591dac7acb4bf0f041a97f
Denys Fominykh <fominykhdenis@gmail.com>
2025-02-28 01:09:31 +0200

    add logic to the server action

--- src/actions/create-topic.ts
+++ src/actions/create-topic.ts
@@ -1,5 +1,52 @@
 'use server';
 
-export async function createTopic(): Promise<void> {
-  // TODO: Revalidate home page after creating a topic
+import { auth } from '@/auth';
+import { createTopicSchema } from '@/validation';
+
+type TFormState = {
+  errors?: {
+    name?: string[];
+    description?: string[];
+    _form?: string[];
+  };
+};
+
+export async function createTopic(_prevState: TFormState, formData: FormData): Promise<TFormState> {
+  try {
+    const session = await auth();
+    const name = formData.get('name') as string;
+    const description = formData.get('description') as string;
+    const validation = createTopicSchema.safeParse({
+      name,
+      description,
+    });
+
+    if (!validation.success) {
+      return { errors: validation.error.flatten().fieldErrors };
+    }
+
+    if (!session || !session?.user) {
+      return {
+        errors: {
+          _form: ['You must be sign in to do this.'],
+        },
+      };
+    }
+
+    return {};
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      return {
+        errors: {
+          _form: [error.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong'],
+        },
+      };
+    }
+  }
 }
