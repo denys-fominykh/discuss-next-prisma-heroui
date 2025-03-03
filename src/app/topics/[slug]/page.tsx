6551697668027e22fefc0868d30bd9f41bf74765
Denys Fominykh <fominykhdenis@gmail.com>
2025-03-03 02:39:04 +0200

    create topic show page

--- src/app/topics/[slug]/page.tsx
+++ src/app/topics/[slug]/page.tsx
@@ -1,3 +1,17 @@
-export default function TopicShowPage() {
-  return <div>Topic Show Page</div>;
+type TTopicShowPageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function TopicShowPage({ params }: TTopicShowPageProps) {
+  const slug = (await params).slug;
+
+  return (
+    <div className="grid grid-cols-4 gap-4 p-4">
+      <div className="col-span-3">
+        <h1 className="mb-2 text-2xl font-bold">{slug}</h1>
+      </div>
+    </div>
+  );
 }
