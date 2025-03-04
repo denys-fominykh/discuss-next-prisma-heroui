c1ed477a4bcf1f6f0092cb979e8da3e882ac4994
Denys Fominykh <fominykhdenis@gmail.com>
2025-03-05 01:43:07 +0200

    implement server action for creating comments

--- src/actions/create-comment.ts
+++ src/actions/create-comment.ts
@@ -1,5 +1,93 @@
 'use server';
 
-export async function createComment(): Promise<void> {
-  // TODO: revalidate post show page
+import { revalidatePath } from 'next/cache';
+
+import { auth } from '@/auth';
+import { db } from '@/db';
+import { createCommentSchema } from '@/validation';
+import paths from '@/paths';
+
+type TCreateCommentParams = {
+  postId: string;
+  parentId?: string;
+};
+
+type TFormState = {
+  errors: {
+    content?: string[];
+    _form?: string[];
+  };
+  success?: boolean;
+};
+
+export async function createComment(
+  { postId, parentId }: TCreateCommentParams,
+  _prevState: TFormState,
+  formData: FormData,
+): Promise<TFormState> {
+  const session = await auth();
+
+  const content = formData.get('content') as string;
+
+  const validation = createCommentSchema.safeParse({
+    content: content,
+  });
+
+  if (!validation.success) {
+    return {
+      errors: validation.error.flatten().fieldErrors,
+    };
+  }
+
+  if (!session || !session.user || !session.user.id) {
+    return {
+      errors: {
+        _form: ['You must sign in to do this.'],
+      },
+    };
+  }
+
+  try {
+    await db.comment.create({
+      data: {
+        content: validation.data.content,
+        postId: postId,
+        parentId: parentId,
+        userId: session.user.id,
+      },
+    });
+  } catch (err) {
+    if (err instanceof Error) {
+      return {
+        errors: {
+          _form: [err.message],
+        },
+      };
+    } else {
+      return {
+        errors: {
+          _form: ['Something went wrong...'],
+        },
+      };
+    }
+  }
+
+  const topic = await db.topic.findFirst({
+    where: { posts: { some: { id: postId } } },
+  });
+
+  if (!topic) {
+    return {
+      errors: {
+        _form: ['Failed to revalidate topic'],
+      },
+    };
+  }
+
+  revalidatePath(paths.postShow(topic.slug, postId));
+
+  return {
+    errors: {},
+    success: true,
+  };
 }
