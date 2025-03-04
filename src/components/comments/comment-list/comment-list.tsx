2da4661687f3cc5f08eb5665c70cdd2503848e6a
Denys Fominykh <fominykhdenis@gmail.com>
2025-03-05 01:39:36 +0200

    add comment list component

---
+++ src/components/comments/comment-list/comment-list.tsx
@@ -0,0 +1,19 @@
+// import { CommentShow } from '@/components/comments';
+
+// type TCommentListProps = {};
+
+// TODO: Get a list of comments from somewhere
+export default function CommentList() {
+  // const topLevelComments = comments.filter((comment) => comment.parentId === null);
+  //
+  // return (
+  //   <div className="space-y-3">
+  //     <h1 className="text-lg font-bold">All {comments.length} comments</h1>
+  //     {topLevelComments.map((comment) => (
+  //       <CommentShow key={comment.id} commentId={comment.id} comments={comments} />
+  //     ))}
+  //   </div>
+  // );
+
+  return <div>Comment List</div>;
+}
