82e0e75a4b3fc47a75ab9e32f28f3d883b235739
Denys Fominykh <fominykhdenis@gmail.com>
2025-03-26 01:44:55 +0200

    integrate PostList component to display top posts using fetchTopPosts function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import { Divider } from '@heroui/divider';
 
+import { PostList } from '@/components/posts';
 import { TopicCreateForm, TopicList } from '@/components/topics';
+import { fetchTopPosts } from '@/db/queries/posts';
 
 export default function HomePage() {
   return (
     <div className="grid grid-cols-4 gap-4 p-4">
       <div className="col-span-3">
         <h1 className="m-2 text-xl">Top Posts</h1>
+        <PostList fetchData={fetchTopPosts} />
       </div>
       <div className="border px-2 py-3 shadow">
         <TopicCreateForm />
